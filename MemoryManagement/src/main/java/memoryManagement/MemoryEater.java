0509455bd04177e4627faa1d5c56086f6d5a83bf
Andrei Buldyshau <andrei_buldyshau@epam.com>
2014-10-29 10:35:32 -0300

    MemmoryEaterFix

--- MemoryManagement/src/main/java/memoryManagement/MemoryEater.java
+++ MemoryManagement/src/main/java/memoryManagement/MemoryEater.java
@@ -10,16 +10,17 @@ public class MemoryEater {
     static final Logger logger = Logger.getLogger(MemoryEater.class);
 
     public static void main(String[] args) {
-    	//This is heap space OutOfMemoryException. We should not make hard reference to objects
-    	//Objects stay in OLD generation.
+        // This is heap space OutOfMemoryException. We should not make hard
+        // reference to objects
+        // Objects stay in OLD generation.
         List v = new ArrayList();
         while (true) {
-        	if(v.size() > 100){
-        		byte b[] = new byte[1048576];
+            if (v.size() < 100) {
+                byte b[] = new byte[1048576];
                 v.add(b);
             }
             Runtime rt = Runtime.getRuntime();
-            
+
             System.out.println("free memory: " + rt.freeMemory());
         }
     }
