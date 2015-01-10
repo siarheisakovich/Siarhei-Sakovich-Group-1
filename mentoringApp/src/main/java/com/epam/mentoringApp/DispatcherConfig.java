3b3af38f7e705d58b2b9e0098ddc54d23c45a1bf
Andrei Buldyshau <andrei_buldyshau@epam.com>
2015-01-10 12:18:42 -0300

    Security

--- mentoringApp/src/main/java/com/epam/mentoringApp/DispatcherConfig.java
+++ mentoringApp/src/main/java/com/epam/mentoringApp/DispatcherConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -12,7 +11,6 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-@EnableWebMvcSecurity
 @ComponentScan("com.epam.mentoringApp.controllers")
 @Import(SecurityConfig.class)
 public class DispatcherConfig extends WebMvcConfigurerAdapter {    
---
+++ mentoringApp/src/main/java/com/epam/mentoringApp/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.epam.mentoringApp;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+    //do nothing
+}
