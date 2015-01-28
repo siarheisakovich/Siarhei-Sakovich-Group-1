b669bcc7b3d58e0b763304bc54704952bdef487c
Andrei Buldyshau <andrei_buldyshau@epam.com>
2015-01-28 13:06:06 -0300

    JMS Initall commit

---
+++ jms-producer/src/main/java/com/epam/mentoringApp/jms/Sender.java
@@ -0,0 +1,48 @@
+package com.epam.mentoringApp.jms;
+
+import javax.jms.DeliveryMode;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+
+public class Sender {
+    public static void main(String[] args) throws Exception {
+        // get the initial context
+        InitialContext ctx = new InitialContext();
+
+        // lookup the queue object
+        Queue queue = (Queue) ctx.lookup("queue/queue0");
+
+        // lookup the queue connection factory
+        QueueConnectionFactory connFactory = (QueueConnectionFactory) ctx
+                .lookup("queue/connectionFactory");
+
+        // create a queue connection
+        QueueConnection queueConn = connFactory.createQueueConnection();
+
+        // create a queue session
+        QueueSession queueSession = queueConn.createQueueSession(false,
+                Session.DUPS_OK_ACKNOWLEDGE);
+
+        // create a queue sender
+        QueueSender queueSender = queueSession.createSender(queue);
+        queueSender.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+        // create a simple message to say "Hello"
+        TextMessage message = queueSession.createTextMessage("Hello");
+
+        // send the message
+        queueSender.send(message);
+
+        // print what we did
+        System.out.println("sent: " + message.getText());
+
+        // close the queue connection
+        queueConn.close();
+    }
+}
